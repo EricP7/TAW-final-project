8ca1375f0d5c3321f81063621f48dd1112c0dac6
eric <ericpredit@gmail.com>
2025-11-11 21:54:10 +0200

    added header components (blank)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup></script>
+<script setup>
+import Header from "./components/Header.vue";
+import LandingPage from "./components/LandingPage.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <Header></Header>
+
+    <title>TAW Final Project</title>
+    <h1>App.vue</h1>
+
+    <LandingPage></LandingPage>
+
+    <p>
+        Visit
+        <a href="https://vuejs.org/" target="_blank" rel="noopener"
+            >vuejs.org</a
+        >
+        to read the documentation
+    </p>
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <header>
+        <p>logo name</p>
+        <button>Pricing</button>
+        <button>FAQ</button>
+        <button>About Us</button>
+    </header>
+</template>
+
+<style></style>
