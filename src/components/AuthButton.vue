35cd378d09bc3a73bbf0fb450b7bd08510ab1bc2
eric <ericpredit@gmail.com>
2026-02-11 10:30:21 +0000

    checks if user is authenticated & changes the button values dynamicly

---
+++ src/components/AuthButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useAuth } from '@/stores/auth';
+import { computed } from 'vue';
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue';
+
+const authStore = useAuth();
+
+// checks from auath.js store if the user is authenticated
+// and uses computed to update whenever the value is changed
+const isAuthenticated = computed(() => authStore.isAuthenticated)
+
+
+// NOT FINISHED
+// need to somehow extract the username (maybe from localStorage or pinia store)
+const userName = computed(() => authStore.VALID_USERNAME)
+
+
+// sets the label and route of the button 
+const authButtonContent = ref(
+    {
+        label: isAuthenticated.value ? 'UserName' : 'Login',
+        route: isAuthenticated.value ? 'UserName' : 'sign-in'
+    }
+)
+
+</script>
+
+<template>
+    <!-- checks if the user is authenticated and changes the button dynamicly-->
+    <div>
+        <RouterLink :to="authButtonContent.route">{{ isAuthenticated ? 'UserName' : 'Login' }}</RouterLink>
+    </div>
+</template>
